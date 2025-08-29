a816600e342cea65ddb0d6ffe0d8c0b3c91242a1
aselharacci <asel.haracci@gmail.com>
2025-08-29 17:15:25 +0300

    video yüklendi

--- src/components/about/Stats.jsx
+++ src/components/about/Stats.jsx
@@ -40,7 +40,7 @@ export default function Stats() {
 					playsInline
 					className="w-full h-full object-cover"
 				>
-					<source src="/videos/digital-marketing.mp4" type="video/mp4" />
+					<source src="/public/stats_video.mp4" type="video/mp4" />
 					Your browser does not support the video tag.
 				</video>
 			</div>
