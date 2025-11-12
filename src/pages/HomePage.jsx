c0f4c6fca1dbc9e2e59fe5e3a54d6fdeab323b9e
aselharacci <asel.haracci@gmail.com>
2025-11-12 17:42:33 +0300

    Add CallToAction component and update HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,8 @@ import CategoryPick from "../components/home-components/CategoryPick";
 import HeroSlider from "../components/home-components/HeroSlider";
 import FavProducts from "../components/home-components/FavProducts";
 import BottomSlider from "../components/home-components/BottomSlider";
-import CallToAction from "../components/home-components/CallToAction";
 import FeaturedPosts from "../components/home-components/FeaturedPosts";
+import CallToAction from "../components/home-components/CallToAction";
 
 export default function Home() {
 	return (
